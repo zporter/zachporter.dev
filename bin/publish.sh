49ec457f5ca3205405fc5fd779c93927c5379e5c
Zachary Porter <zporter9@gmail.com>
2020-08-15 09:28:40 -0400

    Update theme to latest

--- bin/publish.sh
+++ bin/publish.sh
@@ -18,6 +18,9 @@ git worktree add -B gh-pages public origin/gh-pages
 echo "Removing existing files"
 rm -rf public/*
 
+echo "Installing NodeJS dependencies"
+npm install
+
 echo "Generating site"
 hugo
 
